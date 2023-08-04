c208b49c99782be7f5f3091f004e5bfa207f7eeb
Mayank <palmayank2100@gmail.com>
2023-08-04 17:05:20 +0530

    Updates

---
+++ precache-manifest.f603f4f43b6cfec939aa3da746256ee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "139e89c033bb8f7ead85",
+    "url": "/textutils2/static/css/main.1ae9972a.chunk.css"
+  },
+  {
+    "revision": "139e89c033bb8f7ead85",
+    "url": "/textutils2/static/js/main.139e89c0.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/textutils2/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "3bdb33d47e51ddc2d15c",
+    "url": "/textutils2/static/js/2.3bdb33d4.chunk.js"
+  },
+  {
+    "revision": "f714a4e7c273b14f2e24",
+    "url": "/textutils2/static/js/runtime~main.f714a4e7.js"
+  },
+  {
+    "revision": "b988946143ec815b96df5d7294300e47",
+    "url": "/textutils2/index.html"
+  }
+];
