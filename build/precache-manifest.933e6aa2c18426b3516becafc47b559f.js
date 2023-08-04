23eddd155b5051b4ad027d16d5eaaa9b4460df5f
Mayank <palmayank2100@gmail.com>
2023-08-04 18:28:24 +0530

    Fix mobile background issue

---
+++ build/precache-manifest.933e6aa2c18426b3516becafc47b559f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4498007119f12d0ac385",
+    "url": "/textutils2/static/css/main.475b7a6f.chunk.css"
+  },
+  {
+    "revision": "4498007119f12d0ac385",
+    "url": "/textutils2/static/js/main.44980071.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/textutils2/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "3bdb33d47e51ddc2d15c",
+    "url": "/textutils2/static/js/2.3bdb33d4.chunk.js"
+  },
+  {
+    "revision": "f714a4e7c273b14f2e24",
+    "url": "/textutils2/static/js/runtime~main.f714a4e7.js"
+  },
+  {
+    "revision": "f15bfa125cf6afac31d1ec7a8993a664",
+    "url": "/textutils2/index.html"
+  }
+];
