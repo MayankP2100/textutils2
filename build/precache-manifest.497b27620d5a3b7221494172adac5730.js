ddf08540d017758668e079d8626f54ccd345d72a
Mayank <palmayank2100@gmail.com>
2023-08-04 18:04:05 +0530

    Change Dark Button Theme for deployment

---
+++ build/precache-manifest.497b27620d5a3b7221494172adac5730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7d8b2df4585158e3bd8",
+    "url": "/textutils2/static/css/main.475b7a6f.chunk.css"
+  },
+  {
+    "revision": "b7d8b2df4585158e3bd8",
+    "url": "/textutils2/static/js/main.b7d8b2df.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/textutils2/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "3bdb33d47e51ddc2d15c",
+    "url": "/textutils2/static/js/2.3bdb33d4.chunk.js"
+  },
+  {
+    "revision": "f714a4e7c273b14f2e24",
+    "url": "/textutils2/static/js/runtime~main.f714a4e7.js"
+  },
+  {
+    "revision": "c7fb8eefbb5f8f23ec22aa9ad97e9df8",
+    "url": "/textutils2/index.html"
+  }
+];
